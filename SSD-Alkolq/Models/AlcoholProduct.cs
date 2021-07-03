09d9fe15aba4a481deda5d343cdfacc999bcf682
elsa <test@gmail.com>
2021-07-03 21:12:46 +0800

    Changed 'Brand' tp 'Type ' in Alcohol Products page

--- SSD-Alkolq/Models/AlcoholProduct.cs
+++ SSD-Alkolq/Models/AlcoholProduct.cs
@@ -7,7 +7,7 @@ namespace SSD_Alkolq.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Brand { get; set; }
+        public string Type { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
