61e01d03e702e22d4eeef2000e7f298516cdf578
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-10 23:33:55 +0800

    Support editing of product image.

--- SSD-Alkolq/Pages/AlcoholProducts/Edit.cshtml.cs
+++ SSD-Alkolq/Pages/AlcoholProducts/Edit.cshtml.cs
@@ -9,21 +9,29 @@ using Microsoft.EntityFrameworkCore;
 using SSD_Alkolq.Data;
 using SSD_Alkolq.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace SSD_Alkolq.Pages.AlcoholProducts
 {
-    [Authorize(Roles = "Admin")]
+    // [Authorize(Roles = "Admin")]
     public class EditModel : PageModel
     {
         private readonly SSD_Alkolq.Data.AlkolqContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public EditModel(SSD_Alkolq.Data.AlkolqContext context)
+        public EditModel(SSD_Alkolq.Data.AlkolqContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
 
         [BindProperty]
         public Models.AlcoholProduct AlcoholProduct { get; set; }
+
+        [BindProperty]
+        public IFormFile Image { set; get; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -50,6 +58,16 @@ namespace SSD_Alkolq.Pages.AlcoholProducts
                 return Page();
             }
 
+            // Save image to uploads folder.
+            if (Image != null)
+            {
+                var fileName = GenerateUniqueName(this.Image.FileName);
+                var uploadsPath = Path.Combine(_environment.WebRootPath, "images");
+                var filePath = Path.Combine(uploadsPath, fileName);
+                Image.CopyTo(new FileStream(filePath, FileMode.Create));
+                AlcoholProduct.ImageName = fileName;
+            }
+
             _context.Attach(AlcoholProduct).State = EntityState.Modified;
 
             try
@@ -74,6 +92,15 @@ namespace SSD_Alkolq.Pages.AlcoholProducts
         private bool AlchoholProductExists(int id)
         {
             return _context.AlcoholProduct.Any(e => e.ID == id);
+        }
+
+        private string GenerateUniqueName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return Path.GetFileNameWithoutExtension(fileName)
+                   + "_"
+                   + Guid.NewGuid().ToString().Substring(0, 4)
+                   + Path.GetExtension(fileName);
         }
     }
 }
