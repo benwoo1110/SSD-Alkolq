0e3eb2a4f6768cd70539f25a55c9db244e62fca5
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-10 13:28:24 +0800

    Implement basic data seeding from csv.

---
+++ SSD-Alkolq/Migrations/20210710041245_FixSpellingIssue.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSD_Alkolq.Migrations
+{
+    public partial class FixSpellingIssue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AlchoholProduct",
+                table: "AlchoholProduct");
+
+            migrationBuilder.RenameTable(
+                name: "AlchoholProduct",
+                newName: "AlcoholProduct");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AlcoholProduct",
+                table: "AlcoholProduct",
+                column: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AlcoholProduct",
+                table: "AlcoholProduct");
+
+            migrationBuilder.RenameTable(
+                name: "AlcoholProduct",
+                newName: "AlchoholProduct");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AlchoholProduct",
+                table: "AlchoholProduct",
+                column: "ID");
+        }
+    }
+}
