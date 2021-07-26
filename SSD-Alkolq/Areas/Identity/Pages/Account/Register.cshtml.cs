bb871ce231c8f1764bea77761073cb5298e5acac
elsa <test@gmail.com>
2021-07-26 14:13:10 +0800

    Fixed redirection to register email page

--- SSD-Alkolq/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SSD-Alkolq/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -95,6 +95,8 @@ namespace SSD_Alkolq.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                    _userManager.Options.SignIn.RequireConfirmedAccount = true;
+
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
