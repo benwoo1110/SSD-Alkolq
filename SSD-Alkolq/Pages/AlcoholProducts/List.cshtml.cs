d7a42b7abc6115b88037fbf84f608adcd37e7b24
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-27 22:50:06 +0800

    Implement search.

--- SSD-Alkolq/Pages/AlcoholProducts/List.cshtml.cs
+++ SSD-Alkolq/Pages/AlcoholProducts/List.cshtml.cs
@@ -6,35 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using SSD_Alkolq.Data;
+using SSD_Alkolq.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SSD_Alkolq.Pages.AlcoholProducts
 {
-    public class IndexModel : PageModel
+    //[Authorize(Roles = "Admin, Users")]
+    public class ListModel : PageModel
     {
         private readonly SSD_Alkolq.Data.AlkolqContext _context;
 
-        public IndexModel(SSD_Alkolq.Data.AlkolqContext context)
+        public ListModel(SSD_Alkolq.Data.AlkolqContext context)
         {
             _context = context;
         }
 
-        public IList<Models.AlcoholProduct> AlcoholProducts { get; set; }
-        
+        public IList<Models.AlcoholProduct> AlchoholProduct { get;set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
-        
         public SelectList Type { get; set; }
-        
         [BindProperty(SupportsGet = true)]
         public string AlcoholType { get; set; }
-        
         public async Task OnGetAsync()
         {
             IQueryable<string> typeQuery = from m in _context.AlcoholProducts
-                                           orderby m.Type
-                                           select m.Type;
+                                            orderby m.Type
+                                            select m.Type;
             var alcohol = from m in _context.AlcoholProducts
-                          select m;
+                         select m;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 alcohol = alcohol.Where(s => s.Name.Contains(SearchString));
@@ -44,7 +44,7 @@ namespace SSD_Alkolq.Pages.AlcoholProducts
                 alcohol = alcohol.Where(x => x.Type == AlcoholType);
             }
             Type = new SelectList(await typeQuery.Distinct().ToListAsync());
-            AlcoholProducts = await alcohol.ToListAsync();
+            AlchoholProduct = await alcohol.ToListAsync();
         }
     }
 }
