31f683a7c2d1d3bcdf15f9011dcb99c1d2a83fae
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-27 21:32:00 +0800

    Data annotations for ShoppingCartItem.

--- SSD-Alkolq/Models/ShoppingCartItem.cs
+++ SSD-Alkolq/Models/ShoppingCartItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,12 +10,19 @@ namespace SSD_Alkolq.Models
     public class ShoppingCartItem
     {
         public int ID { get; set; }
+
         [ForeignKey("ApplicationUser")]
         public string UserID { get; set; }
+
         public ApplicationUser User { get; set; }
+
         [ForeignKey("AlcoholProduct")]
         public int AlcoholProductID { get; set; }
+
+        [Display(Name = "Product")]
         public AlcoholProduct AlcoholProduct { get; set; }
+
+        [Range(1, 20)]
         public int Quantity { get; set; }
     }
 }
