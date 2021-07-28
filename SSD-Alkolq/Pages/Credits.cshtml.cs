e528fbb4b1809e1a6c7609cbf39398298e94130f
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-28 16:00:28 +0800

    Add basic credits page.

---
+++ SSD-Alkolq/Pages/Credits.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SSD_Alkolq.Pages
+{
+    public class CreditsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
