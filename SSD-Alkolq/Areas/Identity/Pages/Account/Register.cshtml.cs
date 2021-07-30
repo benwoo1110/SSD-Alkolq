f726969b32d7fdfb3b242801da0dfc8cb025a674
elsa <test@gmail.com>
2021-07-30 17:16:54 +0800

    Changed validation error message for Register page

--- SSD-Alkolq/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SSD-Alkolq/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -57,7 +57,7 @@ namespace SSD_Alkolq.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and must contain at least one special character.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
