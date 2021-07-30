27a81451b69559e6a3a9fed29dd43a842f435a4e
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-07-30 18:20:47 +0800

    update email confirm and add payment receipt.

--- SSD-Alkolq/Pages/Payment/Success.cshtml.cs
+++ SSD-Alkolq/Pages/Payment/Success.cshtml.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -14,11 +18,21 @@ namespace SSD_Alkolq.Pages.Payment
 {
     public class SuccessModel : PageModel
     {
+        private readonly IWebHostEnvironment _environment;
+        private readonly IEmailSender _emailSender;
         private readonly SSD_Alkolq.Data.AlkolqContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public SuccessModel(SSD_Alkolq.Data.AlkolqContext context)
+        public SuccessModel(
+            IWebHostEnvironment environment,
+            IEmailSender emailSender,
+            SSD_Alkolq.Data.AlkolqContext context,
+            UserManager<ApplicationUser> userManager)
         {
+            _environment = environment;
+            _emailSender = emailSender;
             _context = context;
+            _userManager = userManager;
         }
 
         public Customer Customer { get; set; }
@@ -53,6 +67,12 @@ namespace SSD_Alkolq.Pages.Payment
             _context.ShoppingCart.RemoveRange(await _context.ShoppingCart
                 .Where(s => s.UserID.Equals(userId))
                 .ToListAsync());
+
+            // Send receipt to email
+            ApplicationUser user = await _userManager.FindByIdAsync(userId);
+            var receiptHtmlPath = Path.Combine(_environment.WebRootPath, "html/paymentreceipt.html");
+            string receiptHtmlText = System.IO.File.ReadAllText(receiptHtmlPath);
+            await _emailSender.SendEmailAsync(user.Email, "Payment receipt", receiptHtmlText);
 
             await _context.SaveChangesAsync();
         }
