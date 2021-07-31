25fa5e463b71fd8309d9fb1a7b63ab81c7c6d2dd
CodyJeow <86243622+CodyJeow@users.noreply.github.com>
2021-07-31 22:14:09 +0800

    Added Authentication for Audit

--- SSD-Alkolq/Pages/AuditLogs/Delete.cshtml.cs
+++ SSD-Alkolq/Pages/AuditLogs/Delete.cshtml.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using SSD_Alkolq.Data;
 using SSD_Alkolq.Models;
 
 namespace SSD_Alkolq.Pages.AuditLogs
 {
+    [Authorize(Roles = "Admin")]
     public class DeleteModel : PageModel
     {
         private readonly SSD_Alkolq.Data.AlkolqContext _context;
